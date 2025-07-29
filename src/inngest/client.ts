4f3f22cfd26ec30954c8842b752f3ccb9a7c7c33
Farhan Ahmad <farhanahmadptn@gmail.com>
2025-07-29 22:24:29 +0530

    changes done

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -5,7 +5,7 @@ export const inngest = new Inngest({
   id: "ai-therapy-agent",
   // You can add your Inngest signing key here if you have one
   eventKey:
-    "Smzg-08plP_p4P1T9R6UA00_gbW4eDi8B-Ay_F6DCB5OYIWqHcAMbGLNRVgLCCJDS3YFth0D3_P3FzPO4vpl0w",
+    "keAacm32Tlf4L4HgaVnxdFWuArxJnhTtiukxAgF3YakrWpHAY53Vb3B7xmXSiALyr-53d0Rm1RzAatW_KGLxlQ",
 });
 
 // Export the functions array (this will be populated by the functions.ts file)
